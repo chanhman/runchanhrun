2274479b550426ec4a3e138789c1869fd67e67e1
Chanh Nguyen <hello@chanh.design>
2021-11-12 22:16:46 -0700

    add function to get monday

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,14 +3,27 @@ import useSwr from 'swr'
 import type { DayType } from 'types'
 import Day from 'components/Day'
 
-const fetcher = (url:string) => fetch(url).then((res) => res.json())
+const fetcher = (url: string) => fetch(url).then((res) => res.json())
 
 const Home: NextPage = () => {
-  const {data = [], error} = useSwr('api/schedule', fetcher)
+  const { data = [], error } = useSwr('api/schedule', fetcher)
 
-  let todaysDate = new Date().toLocaleDateString('en-US').split('/')
-  let todaysDateFormatted = `${todaysDate[2]}-${todaysDate[0]}-${todaysDate[1]}`
-  let todaysWorkout = data.find((day: DayType) => day.Date === todaysDateFormatted)
+  const todaysDate = new Date().toLocaleDateString('en-US').split('/')
+  const todaysDateFormatted = `${todaysDate[2]}-${todaysDate[0]}-${todaysDate[1]}`
+  const mondaysDate = setToMonday(new Date()).toLocaleDateString('en-US').split('/')
+  const mondaysDateFormatted = `${mondaysDate[2]}-${mondaysDate[0]}-${mondaysDate[1]}`
+
+  const todaysWorkout = data.find(
+    (day: DayType) => day.Date === todaysDateFormatted
+  )
+
+  function setToMonday(date: Date) {
+    const day = date.getDay() || 7
+
+    if (day !== 1) date.setHours(-24 * (day - 1))
+
+    return date
+  }
 
   return (
     <main>
